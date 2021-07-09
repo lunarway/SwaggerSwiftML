89234a8d05ae1741e05ba6045c289147cd3e1d90
Mads Bøgeskov <mbo@lunar.app>
2021-07-09 12:05:03 +0200

    Supporting referenced responses

--- Sources/SwaggerSwiftML/Models/Node.swift
+++ Sources/SwaggerSwiftML/Models/Node.swift
@@ -1,4 +1,18 @@
-public enum Node<T> {
+import Foundation
+
+public enum Node<T: Decodable>: Decodable {
     case reference(String)
     case node(T)
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let ref = try? container.decode(Reference.self) {
+            self = .reference(ref.ref)
+        } else if let prop = try? container.decode(T.self) {
+            self = .node(prop)
+        } else {
+            throw DecodingError.valueNotFound(Node.self, .init(codingPath: decoder.codingPath, debugDescription: "Failed to decode node"))
+        }
+    }
 }
